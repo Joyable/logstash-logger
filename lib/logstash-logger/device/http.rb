f58c05c6038bdc6f165d3ebf4b7f26cbfc6fdb71
Kevin Kuchta <kevin@kevinkuchta.com>
2016-11-30 00:11:13 -0800

    Fix up http logging

--- lib/logstash-logger/device/http.rb
+++ lib/logstash-logger/device/http.rb
@@ -15,22 +15,50 @@ module LogStashLogger
         @host = opts[:host] || DEFAULT_HOST
         @http = Net::HTTP.new(@host, @port)
         @http.use_ssl = opts[:use_ssl] || opts[:ssl_enable]
+        @ending = false
 
         @queue = Queue.new
 
-        Thread.new do
+        @log_thread = Thread.new do
           loop do
-            request = Net::HTTP::Post.new("/", {'Content-Type': 'application/json'})
-            request.body = @queue.deq
-            binding.pry
+            message, time = @queue.deq
+            request = Net::HTTP::Post.new('/', {'Content-Type': 'application/json'})
+
+            # Try to match the fields provided by aptible's logdrain
+            request.body = {
+              '@timestamp': time.iso8601(3),
+              app: 'joyable-rails',
+              layer: 'app',
+              log: message,
+              service: 'joyable-rails-web', # TODO
+              source: 'app',
+              stream: 'stdout',
+              time: time.iso8601(9),
+              type: 'json'
+            }.to_json
+            puts "POSTING #{request.body}"
             @http.request(request)
+
+            if @ending && @queue.empty?
+              break
+            end
+          end
+        end
+
+        # Finish sending queued log message before closing rake / console
+        at_exit do
+          unless @queue.empty?
+            puts "Waiting for log queue to empty..."
+            @ending = true
+            @log_thread.join
           end
         end
       end
 
       def write_one(message)
-        @queue.enq message
+        @queue.enq [message, Time.current.utc]
       end
     end
   end
 end
+
